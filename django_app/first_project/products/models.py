be991b52cc6d6b719f66279757330c300b7fddee
gunda sanjay <sanjaykrishnagunda13@gmail.com>
2021-06-23 16:56:06 +0530

    updated the settings with postgresql

--- django_app/first_project/products/models.py
+++ django_app/first_project/products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class producs(models.Model):
+class products(models.Model):
     id = models.AutoField(primary_key = True)
     products_name = models.CharField(max_length=20,default="cars")
     user = models.ForeignKey(
--- django_app/first_project/products/views.py
+++ django_app/first_project/products/views.py
@@ -5,23 +5,22 @@ from django.shortcuts import  render, redirect
 from products.forms import NewUserForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm 
-from products.models import producs
+from products.models import products
 from django.contrib.auth.models import User
 
 from django.contrib import messages #import messages
 
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-            
-			return render(request,"home.html", context={'data':["no value"]})
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm
-	return render (request=request, template_name="register.html", context={"register_form":form})
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return render(request,"home.html", context={'data':["no value"]})
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm
+    return render (request=request, template_name="register.html", context={"register_form":form})
 
 
 def login_request(request):
@@ -34,8 +33,8 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}.")
-                products = list_products(user)
-                return render(request,"home.html",context={'data':products})
+                product = list_products(user)
+                return render(request,"home.html",context={'data':product})
             else:
                 messages.error(request,"Invalid username or password.")
         else:
@@ -45,35 +44,32 @@ def login_request(request):
 
 def new_product(request):
     if request.method == 'POST':
-
-        #product = request.method['product']
-        #print(request.User)
         print(request.user)
         print(request.POST['product'])
         abc=""
         if len(request.POST['product'])>0:
             try:
-                products =producs()
-                products.products_name = request.POST['product']
-                products.user = User.objects.get(username=request.user)
-                products.save()
+                prod =products()
+                prod.products_name = request.POST['product']
+                prod.user = User.objects.get(username=request.user)
+                prod.save()
             except Exception as error:
                 print (error)
         
             ### will list product
-        products=list_products(request.user)
+        product=list_products(request.user)
 
 
                 
-        return render(request,"home.html",context={'data':products})
+        return render(request,"home.html",context={'data':product})
 
 
 def list_products(user):
-    abc= list(producs.objects.filter(user=user).values('products_name'))
-    products=[]
+    abc= list(products.objects.filter(user=user).values('products_name'))
+    product=[]
     if len(abc) < 1:
-        products.append("no value")
+        product.append("no value")
     else:
         for i in abc:
-            products.append(i['products_name'])
-    return products
+            product.append(i['products_name'])
+    return product
